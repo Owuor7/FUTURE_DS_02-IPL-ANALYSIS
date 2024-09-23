677f5a708780c7a641f68b0ed44aa6c384915217
Levis Owuor <168531742+Owuor7@users.noreply.github.com>
2024-09-23 12:49:06 +0300

    Add files via upload

---
+++ ipl league.sql
@@ -0,0 +1,11 @@
+----use the database IPL_databse 
+
+use IPL_database
+
+----select the table ipl_matches
+select * from dbo.[ipl_matches_2008_2022 (1)]
+
+
+----select data from ball by ball table 
+select * from [dbo].[ipl_ball_by_ball_2008_2022];
+
